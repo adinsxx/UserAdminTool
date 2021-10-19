d09c7dcb787795ea9bd05bca329d56c03e84f556
Connor <c.mccurrie@gmail.com>
2021-10-19 18:18:08 -0500

    Initial Commit to GH

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserAdminTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
